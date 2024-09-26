f9676f563cd0c480f0a1cc22ceea7ac6d905610c
FervKa <ferbohi@outlook.com>
2024-09-25 22:16:15 -0500

    Working in CardsManager

--- src/main/java/org/classes/CardsManager.java
+++ src/main/java/org/classes/CardsManager.java
@@ -1,59 +1,49 @@
 package org.classes;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonToken;
-
-import java.io.File;
+import javax.smartcardio.Card;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.SimpleFormatter;
 
 public class CardsManager {
+    private List<Card> cards = null;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy/MM");
+    CardsManager() {
+        cards = readCards();
+    }
 
+    private List<Card> readCards() {
+        Path filePath = Paths.get("data/bank_cards.txt");
 
-    public static void main(String[] args) {
-        JsonFactory factory = new JsonFactory();
-        ArrayList<String> cardFields = new ArrayList<>(Arrays.asList("name",
-                "cardNumber",
-                "issurer",
-                "expiryDate",
-                "cvv",
-                "type",
-                "bank"
-        ));
+        try (BufferedReader reader = Files.newBufferedReader(filePath)) {
+            return reader.lines().map(this::buildCards).toList();
 
-        try {
-            JsonParser parser = factory.createParser(new File("data/bank_cards.json"));
-
-            while (!parser.isClosed()) {
-                JsonToken token = parser.nextToken();
-
-                if (token == null) break;
-
-                if (JsonToken.FIELD_NAME.equals(token)) {
-                    String fieldName = parser.getCurrentName();
-
-                    if (cardFields.contains(fieldName)) {
-                        token = parser.nextToken();
-
-                        if (token == JsonToken.VALUE_STRING) {
-                            System.out.println(fieldName + ": " + parser.getText());
-                        } else if (token == JsonToken.VALUE_NUMBER_INT) {
-                            System.out.println(fieldName + ": " + parser.getLongValue());
-                        } else if (token == JsonToken.VALUE_NUMBER_FLOAT) {
-                            System.out.println(fieldName + ": " + parser.getDoubleValue());
-                        } else if (token == JsonToken.VALUE_TRUE || token == JsonToken.VALUE_FALSE) {
-                            System.out.println(fieldName + ": " + parser.getBooleanValue());
-                        }
-                    }
-                }
-            }
-
-            parser.close();
-
-        } catch (IOException e) {
+        }catch (IOException e) {
             e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
+    /*Dora, Borer, 4750960927729037, visa, 04/28, 186, credit, bank*/
+
+    private Card buildCards(String line) {
+        String[] cardArray = line.split(",");
+        return new Card(
+                cardArray[0].trim(),
+                cardArray[1].trim(),
+                cardArray[2].trim(),
+                LocalDate(cardArray[3].trim()).ofPattern(),
+                cardArray[4].trim(),
+                cardArray[5].trim(),
+                cardArray[6].trim(),
+                cardArray[7].trim()
+                )
+    }
+
 }
