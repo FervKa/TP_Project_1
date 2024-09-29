75296e9e14e4654c2e2981129edd5f015b517dfa
FervKa <ferbohi@outlook.com>
2024-09-29 13:41:10 -0500

    Working in the CardMaganer

--- src/main/java/org/classes/CardsManager.java
+++ src/main/java/org/classes/CardsManager.java
@@ -1,50 +1,81 @@
 package org.classes;
 
-import javax.smartcardio.Card;
+import org.interfaces.CardRepository;
+import org.records.Card;
+import org.records.CardType;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.logging.SimpleFormatter;
 
-public class CardsManager {
+public class CardsManager implements CardRepository {
     private List<Card> cards = null;
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy/MM");
-    CardsManager() {
+    SimpleDateFormat sdf = new SimpleDateFormat("yy/MM");
+
+    public CardsManager() {
         cards = readCards();
     }
 
-    private List<Card> readCards() {
+    public List<org.records.Card> readCards() {
         Path filePath = Paths.get("data/bank_cards.txt");
 
         try (BufferedReader reader = Files.newBufferedReader(filePath)) {
-            return reader.lines().map(this::buildCards).toList();
+            cards = reader.lines().map(this::buildCards).toList();
+            return cards;
 
-        }catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
-    /*Dora, Borer, 4750960927729037, visa, 04/28, 186, credit, bank*/
 
-    private Card buildCards(String line) {
+    public org.records.Card buildCards(String line) {
         String[] cardArray = line.split(",");
-        var dataDate = cardArray[3].split("/");
-        return new Card(
-                cardArray[0].trim(),
-                cardArray[1].trim(),
-                cardArray[2].trim(),
-                /*new LocalDate(dataDate[0], dataDate[1]),*/ // Need to refactor the date.
-                cardArray[4].trim(),
-                cardArray[5].trim(),
-                cardArray[6].trim(),
-                cardArray[7].trim()
-                )
+        Date date = null;
+        try {
+            if (sdf.parse(cardArray[4]) instanceof Date) {
+                date = sdf.parse(cardArray[4]);
+            }
+
+            return new org.records.Card(
+                    cardArray[0].trim(),
+                    cardArray[1].trim(),
+                    Long.parseLong(cardArray[2].trim()),
+                    cardArray[4].trim(),
+                    date,
+                    Integer.parseInt(cardArray[5].trim()),
+                    new CardType(cardArray[6].trim()),
+                    cardArray[7].trim()
+            );
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+
+    @Override
+    public Card findOne(long cardNumber) {
+        return null;
+    }
+
+    @Override
+    public List<Card> find(Date expirateDate) {
+        return List.of();
+    }
+
+    @Override
+    public List<Card> find(String bank) {
+        return List.of();
+    }
+
+    @Override
+    public void add(Card card) {
+
+    }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,10 +1,12 @@
 package org.example;
 
+import org.classes.CardsManager;
 import org.classes.CreditCards;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
     CreditCards creditCards = new CreditCards();
+    CardsManager cardsManager = new CardsManager();
 
     @Test
     public void test() {
@@ -14,4 +16,8 @@ public class MainTest {
 
     /*Type here the tests should be to test all the methods and the rest of the app*/
 
+    @Test
+    public void test2() {
+        System.out.println(cardsManager.readCards());
+    }
 }
