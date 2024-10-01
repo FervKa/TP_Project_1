da11330bb8ab8118d49ffba7e4b0a8f0a3fda8aa
FervKa <ferbohi@outlook.com>
2024-10-01 10:37:57 -0500

    Merge non_prod into fervka

--- src/main/java/org/repository/impl/CardRepositoryImpl.java
+++ src/main/java/org/repository/impl/CardRepositoryImpl.java
@@ -9,15 +9,17 @@ import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.model.CardRecord;
 import org.model.CardTypeRecord;
 import org.repository.CardRepository;
 
-public class CardRepositoryManager implements CardRepository {
+public class CardRepositoryImpl implements CardRepository {
+    protected static final Logger logger = LogManager.getLogger(CardRepositoryImpl.class);
     private List<CardRecord> cards = null;
 
-    public CardRepositoryManager() {
+    public CardRepositoryImpl() {
         cards = readCards();
     }
 
@@ -31,7 +33,10 @@ public class CardRepositoryManager implements CardRepository {
     }
 
     public CardRecord findOne(long cardNumber) {
-        return cards.stream().filter(p -> p.cardNumber() == cardNumber).findFirst().orElse(null);
+        CardRecord result = cards.stream().filter(p -> p.cardNumber() == cardNumber).findFirst().orElse(null);
+        logger.info("Card number found: " + cardNumber);
+        return result;
+
     }
 
     @Override
@@ -42,6 +47,7 @@ public class CardRepositoryManager implements CardRepository {
                 result.add(card);
             }
         }
+        logger.info("Found " + result.size() + " cards with expiry date filter.");
         return result;
     }
 
@@ -53,11 +59,13 @@ public class CardRepositoryManager implements CardRepository {
                 result.add(card);
             }
         }
+        logger.info("Found " + result.size() + " cards with bank filter.");
         return result;
     }
 
     public void add(CardRecord card){
         cards.add(card);
+        logger.info("Added card: " + card.cardNumber());
     }
 
     private CardRecord buildCards(String line) {
